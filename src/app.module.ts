3318f4b6b92fbac95d1f71e452e2a31bb342f67b
khantkyaw6 <khantkaneki@gmail.com>
2025-08-04 17:14:01 +0700

    config feature added for environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://jeweladmin:jewelpassword@cluster0jewel.4arnjoc.mongodb.net/nest?retryWrites=true&w=majority&appName=Cluster0Jewel',
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true, // make ConfigService available everywhere
+      load: [configuration], // load our custom config
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('database.url'),
+      }),
+      inject: [ConfigService],
+    }),
     UsersModule,
     PostsModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
